8c45f53c758a010bfe1e67585549a630a26359f5
Jonathan Harford <jonathan.harford@gmail.com>
2025-06-08 16:37:33 -0700

    Revert "Update Vite config to set base path based on build command"
    This reverts commit d4d45e122f480f5f26098e2a423d8faa3deb132c.

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig(({ command, mode }) => ({
   plugins: [svelte()],
-  base: command === 'build' ? '/skvetchy/' : '/',
+  base: mode === 'production' ? '/skvetchy/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
